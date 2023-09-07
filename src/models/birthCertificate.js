7bb4faf77fd33cb62a8bef3ec03298d9cb27d08d
Flesier <flesieronyango18@gmail.com>
2023-09-08 01:25:55 +0300

    created a data model using mongoose

---
+++ src/models/birthCertificate.js
@@ -0,0 +1,24 @@
+//data model for the birth certificate using mongoose
+const mongoose = require('mongoose');
+
+const birthCertificate = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    placeOfBirth: {
+        type: String,
+        required: true,
+    },
+    maidenName: {
+        type: String,
+        required: true,
+    },
+    // will add more fields if needed
+});
+
+module.exports = mongoose.model('birthCertificate', birthCertificateSchema);
