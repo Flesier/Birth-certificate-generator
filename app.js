e31c359f5b2f34d495a2ebb166084790b6f956dd
Flesier <flesieronyango18@gmail.com>
2023-09-15 19:26:05 +0300

    added POST endpoint

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/birthcertificate', {
     process.exit();
 });
 
-
+//retrieve birth certificates
 app.get('/getbirthcertificates', async (req, res) => {
     try {
         const birthCertificates = await BirthCertificate.find();
@@ -29,6 +29,19 @@ app.get('/getbirthcertificates', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 
+});
+
+
+//create birth certificate
+app.post('/birth-certificates', async (req, res) => {
+    try {
+        const birthCertificate = new BirthCertificate(req.body);
+        await birthCertificate.save();
+        res.send(birthCertificate);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 
