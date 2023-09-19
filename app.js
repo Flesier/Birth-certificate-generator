9ba7f88c864f61a4043f713e5e7c4ad000a64984
Moses-Morris <mosesmorrisdev@gmail.com>
2023-09-19 12:14:01 +0300

    Generate file and Send back to user.

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const BirthCertificate = require('./src/models/birthCertificate');
 const generatePDF = require('./src/utils/pdfGenerator');
+const path = require('path'); // Import the 'path' module
+
 
 require('dotenv').config();
 const fs = require('fs')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+//for form submission in express
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //Oauth 
 const { auth, requiresAuth } = require('express-openid-connect');
@@ -75,23 +83,66 @@ app.get('/getbirthcertificates', async (req, res) => {
 app.post('/birth-certificates', async (req, res) => {
     try {
         const birthCertificateData = req.body;
+
+        // Check if all required fields are present in the request body
+        if (!birthCertificateData.name) {
+            return res.status(400).json({ error: 'Missing required name field' });
+        }
+        if (!birthCertificateData.dateOfBirth) {
+            return res.status(400).json({ error: 'Missing required date of birth field' });
+        }
+        if (!birthCertificateData.placeOfBirth) {
+            return res.status(400).json({ error: 'Missing required place of birth field' });
+        }
+        if (!birthCertificateData.maidenName) {
+            return res.status(400).json({ error: 'Missing required maiden name field' });
+        }
+
+        // Generate the PDF certificate
         const pdfData = await generatePDF(birthCertificateData);
 
         if (pdfData) {
-            const birthCertificate = new BirthCertificate(birthCertificateData);
-            await birthCertificate.save();
-            res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename=birth_certificate.pdf');
-            res.send(pdfData);
+            // Create a valid filename by replacing spaces with underscores
+            const sanitizedName = birthCertificateData.name.replace(/ /g, '_');
+            const filename = `${sanitizedName}.pdf`;
+            const filePath = path.join(__dirname, filename); // Use the current directory
+            // Inside generatePDF function
+            console.log('PDF Data:', pdfData);
+            // Save the PDF data to the unique filename
+            console.log('Filename:', filename);
+            console.log('File Path:', filePath);
+            console.log('Current Directory:', __dirname);
+
+            // Save the PDF data to the unique filename
+            try {
+                fs.writeFileSync(filePath, pdfData);
+                console.log('File written successfully');
+
+                // Set response headers for a PDF file attachment
+                res.setHeader('Content-Type', 'application/pdf');
+                res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+
+                // Read the file from the file system and pipe it to the response
+                const stream = fs.createReadStream(filePath);
+                stream.pipe(res);
+
+                stream.on('end', () => {
+                    // Clean up the temporary PDF file after it has been sent
+                    fs.unlinkSync(filePath);
+                });
+            } catch (err) {
+                console.error('File writing failed:', err);
+                res.status(500).json({ error: 'File writing failed' });
+            }
         } else {
             res.status(500).json({ error: 'PDF generation failed' });
         }
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: err });
     }
-});
-
+}
+);
 
 
 // Routes
