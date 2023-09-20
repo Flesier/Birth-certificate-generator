c754998dab7ba3a976c6a2d07f1724a038e69a79
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-09-20 20:15:06 +0300

    saves and creates pdf

--- app.js
+++ app.js
@@ -26,20 +26,16 @@ const config = {
     baseURL: process.env.BASEURL,
     clientID: process.env.CLIENTID,
     issuerBaseURL: process.env.ISSUER,
-  };
-// auth router attaches /login, /logout, and /callback routes to the baseURL
+};
 app.use(auth(config));
-  
 
 app.use(express.json());
 app.set('view-engine', 'ejs');
 
-// Serve static files (CSS, JavaScript, images, etc.) from a directory
-app.use(express.static('public')); // Replace 'public' with your actual static files directory
+app.use(express.static('public'));
 
 
 
-// connect to the database-MongoDB.connect() method
 mongoose.connect('mongodb://127.0.0.1:27017/birthcertificate', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -50,23 +46,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/birthcertificate', {
     process.exit();
 });
 
-//render homepage
+
+
 app.get('/', (req, res) => {
     console.log(req.oidc.isAuthenticated());
     res.render('index.ejs');
 });
 
-
-//authentication inorder to login users and connect to our form
-
-
-
-//render form page when clicked
 app.get('/form', requiresAuth(), (req, res) => {
     res.render('form.ejs');
 });
 
-//retrieve birth certificates
 app.get('/getbirthcertificates', async (req, res) => {
     try {
         const birthCertificates = await BirthCertificate.find();
@@ -75,16 +65,15 @@ app.get('/getbirthcertificates', async (req, res) => {
         console.log(err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-
 });
 
 
-//create birth certificate
 app.post('/birth-certificates', async (req, res) => {
     try {
         const birthCertificateData = req.body;
+        const birthCertificate = new BirthCertificate(req.body);
+        await birthCertificate.save();
 
-        // Check if all required fields are present in the request body
         if (!birthCertificateData.name) {
             return res.status(400).json({ error: 'Missing required name field' });
         }
@@ -98,36 +87,26 @@ app.post('/birth-certificates', async (req, res) => {
             return res.status(400).json({ error: 'Missing required maiden name field' });
         }
 
-        // Generate the PDF certificate
         const pdfData = await generatePDF(birthCertificateData);
 
         if (pdfData) {
-            // Create a valid filename by replacing spaces with underscores
             const sanitizedName = birthCertificateData.name.replace(/ /g, '_');
             const filename = `${sanitizedName}.pdf`;
-            const filePath = path.join(__dirname, filename); // Use the current directory
-            // Inside generatePDF function
+            const filePath = path.join(__dirname, filename);
             console.log('PDF Data:', pdfData);
-            // Save the PDF data to the unique filename
             console.log('Filename:', filename);
             console.log('File Path:', filePath);
             console.log('Current Directory:', __dirname);
 
-            // Save the PDF data to the unique filename
             try {
                 fs.writeFileSync(filePath, pdfData);
                 console.log('File written successfully');
-
-                // Set response headers for a PDF file attachment
                 res.setHeader('Content-Type', 'application/pdf');
                 res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
 
-                // Read the file from the file system and pipe it to the response
                 const stream = fs.createReadStream(filePath);
                 stream.pipe(res);
-
                 stream.on('end', () => {
-                    // Clean up the temporary PDF file after it has been sent
                     fs.unlinkSync(filePath);
                 });
             } catch (err) {
@@ -145,12 +124,7 @@ app.post('/birth-certificates', async (req, res) => {
 );
 
 
-// Routes
-//const certificatesRouter = require('./src/routes/certificates.js');
-//app.use('/certificates', certificatesRouter);
 
-
-//starting server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
